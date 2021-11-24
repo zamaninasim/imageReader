02a99268e1b9a60a35de25e4b952eeab2c9f3a63
nasim <zamaninasim213@gamail.com>
2021-11-24 19:43:19 +0330

    add impementain of image reader to image decorator

--- src/ir/maktab/decorator/ImageDecorator.java
+++ src/ir/maktab/decorator/ImageDecorator.java
@@ -1,5 +1,14 @@
 package ir.maktab.decorator;
 
-public class ImageDecorator {
+public class ImageDecorator implements ImageReader {
+    ImageReader imageReader;
 
+    public ImageDecorator(ImageReader imageReader) {
+        this.imageReader = imageReader;
+    }
+
+    @Override
+    public void read() {
+        this.imageReader.read();
+    }
 }
