8d0e61ee6439d3b849b53bf36ae1f219c0a4d269
nasim <zamaninasim213@gamail.com>
2021-11-24 19:44:03 +0330

    add override read in basicImageReader

--- src/ir/maktab/decorator/BasicImageReader.java
+++ src/ir/maktab/decorator/BasicImageReader.java
@@ -1,4 +1,8 @@
 package ir.maktab.decorator;
 
-public class BasicImageReader {
+public class BasicImageReader implements ImageReader {
+    @Override
+    public void read() {
+        System.out.print("Your image has been uploaded->>");
+    }
 }
